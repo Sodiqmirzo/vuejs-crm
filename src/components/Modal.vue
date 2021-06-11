8c95bc20658260d2a3ea612a32523af29e331732
Sodiqmirzo Sattorov <mr.sodiqmirzosattorov17@gmail.com>
2021-06-11 20:56:29 +0500

    testing vue

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,9 +1,12 @@
 <template>
-    
+  <div>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis quidem
+    distinctio animi, dolores et earum expedita accusamus assumenda esse
+    voluptates inventore ratione explicabo nobis aperiam delectus at ipsam
+    maxime fugiat?
+  </div>
 </template>
 
 <script>
-export default {
-    
-}
+export default {};
 </script>
